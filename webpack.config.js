46938695e2c5698b214c16bc9f8cbb2cd18e7d01
Mikelangelo64 <donni2001331@gmail.com>
2023-12-05 14:39:31 +0200

    add excavation page

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 const PugPlugin = require('pug-plugin');
 
-const pageNames = ['repair'];
+const pageNames = ['repair', 'excavation'];
 
 const pages = pageNames.map((name) => {
   return {
