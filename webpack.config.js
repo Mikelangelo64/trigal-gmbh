b282f87962ae8e96cde5550da1fb72f689f93502
Mikelangelo64 <donni2001331@gmail.com>
2023-12-12 19:16:00 +0200

    add shop catalog page

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,13 @@ const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 const PugPlugin = require('pug-plugin');
 
-const pageNames = ['repair', 'excavation', 'wallprinting', 'shop-main'];
+const pageNames = [
+  'repair',
+  'excavation',
+  'wallprinting',
+  'shop-main',
+  'shop-catalog'
+];
 
 const pages = pageNames.map((name) => {
   return {
